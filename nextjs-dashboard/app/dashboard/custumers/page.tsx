38944af1fe28a0dfba8b54749f960b8cb1bd53ed
Antonio VelazMtz <tonispartan97@gmail.com>
2026-04-07 06:11:27 +0000

    Dashboard y rutas anidadas

---
+++ nextjs-dashboard/app/dashboard/custumers/page.tsx
@@ -0,0 +1,3 @@
+export default function PanchoVilla(){
+    return <p>Customers Page</p>
+}
--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -10,8 +10,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${montserrat.className}antialiased`}>
         <h1>Un titulo para la pagina bien tituloso</h1>
+          Esto es parte del layout
         <div style= {{ border: '16px solid #000'}}> 
         {children}</div>
+        <footer className='py-10 flex justify-center items-center'>
+          Hecho con amor de un estudiante
+        </footer>
         </body>
     </html>
   );
