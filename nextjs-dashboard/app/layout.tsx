fecaf835107ecf8def4c47632bee54a89d369779
Antonio VelazMtz <tonispartan97@gmail.com>
2026-04-07 04:03:45 +0000

    Seperacion de Layout

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -5,7 +5,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <h1>Un titulo para la pagina bien tituloso</h1>
+        <div style= {{ border: '16px solid #000'}}> 
+        {children}</div>
+        </body>
     </html>
   );
 }
